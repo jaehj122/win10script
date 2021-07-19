9463f3d274b6549f8c7fedbe5cfa6626238e1cd1
jaehj122 <76499500+jaehj122@users.noreply.github.com>
2021-07-18 19:40:03 -0700

    Add files via upload

---
+++ sys_restore_Windows10_Admin_2004_sys-restore-sudo - Copy.ps1
@@ -0,0 +1,8 @@
+﻿Get-WindowsSearchSetting
+Get-WinSystemLocale
+Get-WindowsErrorReporting
+Get-WindowsUpdateLog -Confirm
+Disable-WindowsErrorReporting
+Defender\Start-MpScan -AsJob -ScanType FullScan
+Set-WindowsSearchSetting -EnableMeteredWebResultsSetting yes -EnableWebResultsSetting no -SafeSearchSetting Moderate -SearchExperienceSetting Personalized
+ConfigDefender\Get-MpComputerStatus -AsJob
